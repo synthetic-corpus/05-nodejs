8b63055b6fbc1f57858565de687cb590199c174b
Syn <joel.gonzaga@gmail.com>
2017-09-06 13:04:25 -0700

    wrote a new server app. Works.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+var app = express();
+
+// Set ups handler of http Get request
+app.get('/', (req, res) => {
+  res.send('Hello Express!')
+})
+
+app.get('/bad',(req,res) => {
+  res.send({
+    status: 'bad',
+    reasons:[
+      'this',
+      'empty',
+      'array'
+    ]
+  })
+})
+// Listen on a parituclar port
+app.listen(3000);

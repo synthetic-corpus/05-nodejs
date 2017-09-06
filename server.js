e8e8aa34521cb80bb3e424aeeeced021856d518b
Syn <joel.gonzaga@gmail.com>
2017-09-06 16:13:23 -0700

    made my first rendered page. Yay.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'))
 // Set ups handler of http Get request
 app.get('/', (req, res) => {
   res.send('Hello Express!')

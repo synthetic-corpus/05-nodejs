4fdff61223a5f6e4055bedf69edaaba8f4cbb083
Syn <joel.gonzaga@gmail.com>
2017-09-12 13:10:34 -0700

    added the middle ware. Works okay.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 // Set ups handler of http Get request
+
+// Middleware request.
+// Do not understand quite what middleware is.
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var sayThis = `${now}: ${req.method} ${req.url}`;
+  console.log(sayThis);
+  fs.appendFile('server.log', sayThis +"\n", (err) =>{
+    if (err != null){
+      console.log(err);
+    }
+  });
+  next();
+})
 app.get('/', (req, res) => {
   res.render('home.hbs',{
     pageTitle:'Home Page',
@@ -15,6 +30,9 @@ app.get('/', (req, res) => {
   })
 })
 
+hbs.registerHelper('getYearNow', () => {
+  return new Date().getFullYear();
+})
 app.get('/bad',(req,res) => {
   res.send({
     status: 'bad',
@@ -28,8 +46,7 @@ app.get('/bad',(req,res) => {
 
 app.get('/about',(req,res)=>{
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   })
 })
 // Listen on a parituclar port
